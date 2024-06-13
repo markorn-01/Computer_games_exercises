f2a4500954f0039d3c6748a80ad980958cdfea69
tahaerkoc <taha2409@outlook.de>
2024-06-13 14:54:57 +0200

    tried implementing lineread

--- CG_2024s_Ngo_07/HSH_student/node_2d.gd
+++ CG_2024s_Ngo_07/HSH_student/node_2d.gd
@@ -19,6 +19,7 @@ func _on_edit_point_text_submitted(_new_text):
 	$"Edit Point".text = str(abs(float($"Edit Point".text)))
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -26,3 +27,22 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+
+func _on_edit_segments_lines_edited_from(from_line, to_line):
+	var input_text = $"Edit Segments".text
+	var line = input_text.strip_edges()
+	var segments = line.split("), ")
+	var result = []
+
+	for segment in segments:
+		print("Segment: ", segment)
+	# Extract the coordinate part, assuming it's always after the first space
+		var name_and_coords = segment.split(", ", false, 2)
+		print(name_and_coords)
+		var coords = name_and_coords[1].replace("(", "").replace(")", "")
+		print("coords: ", coords)
+		var coord_list = coords.split(",")
+		#var coords_tuple = [float(coord_list[0]), float(coord_list[1])]
+		#result.append(coords_tuple)
+	print(result)
